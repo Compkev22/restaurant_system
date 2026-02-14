3ca2c37c13c6263c9bdd4954d568cb50718c0a6f
BradOlv <boliva-2023502@kinal.edu.gt>
2026-02-14 01:29:10 -0600

    T32/ Creación del módulo Reservación: Modelo simplificado, rutas y controladores CRUD

---
+++ src/Reservation/reservation.controller.js
@@ -0,0 +1,42 @@
+import Reservation from './reservation.model.js';
+
+export const saveReservation = async (req, res) => {
+    try {
+        const data = req.body;
+        const reservation = new Reservation(data);
+        await reservation.save();
+        return res.status(201).send({ success: true, message: 'Reservación creada' });
+    } catch (err) {
+        return res.status(500).send({ success: false, message: 'Error al reservar', err });
+    }
+};
+
+export const getReservations = async (req, res) => {
+    try {
+        const reservations = await Reservation.find().populate('table', 'numberTable');
+        return res.send({ success: true, reservations });
+    } catch (err) {
+        return res.status(500).send({ success: false, message: 'Error al obtener', error: err.message });
+    }
+};
+
+export const updateReservation = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const updated = await Reservation.findByIdAndUpdate(id, data, { new: true });
+        return res.send({ success: true, message: 'Actualizado', updated });
+    } catch (err) {
+        return res.status(500).send({ success: false, message: 'Error al actualizar' });
+    }
+};
+
+export const deleteReservation = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Reservation.findByIdAndDelete(id);
+        return res.send({ success: true, message: 'Reservación eliminada' });
+    } catch (err) {
+        return res.status(500).send({ success: false, message: 'Error al eliminar' });
+    }
+};
