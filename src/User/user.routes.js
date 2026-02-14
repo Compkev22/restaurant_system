8232f08f87441c10b6952120943fcc4437fa0eeb
DevJosL <jmoran-2022388@kinal.edu.gt>
2026-02-14 09:49:14 -0600

    T9/ Crear la validación para Usuario

--- src/User/user.routes.js
+++ src/User/user.routes.js
@@ -7,22 +7,31 @@ import {
     changeUserStatus
 } from './user.controller.js';
 
+import {
+    validateCreateUser,
+    validateUpdateUserRequest,
+    validateUserStatusChange,
+    validateGetUserById
+} from '../../middlewares/user-validator.js';
+
 const router = Router();
 
 router.get('/', getUsers);
-router.get('/:id', getUserById);
+router.get('/:id', validateGetUserById, getUserById);
 
 router.post(
     '/',
+    validateCreateUser,
     createUser
 );
 
 router.put(
     '/:id',
+    validateUpdateUserRequest,
     updateUser
 );
 
-router.put('/:id/status', changeUserStatus);
+router.put('/:id/status', validateUserStatusChange, changeUserStatus);
 
 
 export default router;
