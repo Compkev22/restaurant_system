e47a9e5d0d3a32e16b22fd7ecaa3c0fa87b1aec4
D1eegoMUI <dlopez-2021700@kinal.edu.gt>
2026-02-14 13:08:04 -0600

    T30/ Creacion de validators para el modulo Pedido

---
+++ middlewares/order.validator.js
@@ -0,0 +1,17 @@
+import { body, param } from 'express-validator';
+import { checkValidators } from './check-validators.js';
+
+export const validateCreateOrder = [
+    body('orderId').isNumeric().withMessage('ID de orden debe ser numérico'),
+    body('detallePedidoId').isNumeric().withMessage('ID de detalle debe ser numérico'),
+    body('mesaId').isNumeric().withMessage('ID de mesa debe ser numérico'),
+    body('empleadoId').isNumeric().withMessage('ID de empleado debe ser numérico'),
+    body('estado').optional().isIn(['Pendiente', 'En Preparacion', 'Listo', 'Entregado', 'Cancelado']),
+    checkValidators
+];
+
+export const validateUpdateStatus = [
+    param('id').isMongoId().withMessage('ID de registro inválido'),
+    body('estado').isIn(['Pendiente', 'En Preparacion', 'Listo', 'Entregado', 'Cancelado']),
+    checkValidators
+];
