4b6a678797baeff8970ef333d9f7bd0aeef4ea94
BradOlv <boliva-2023502@kinal.edu.gt>
2026-02-22 19:17:38 -0600

    T67/ Implementar Soft Delete a la entidad product

--- src/Product/product.routes.js
+++ src/Product/product.routes.js
@@ -10,5 +10,5 @@ const router = Router();
 router.get('/', getProducts);
 router.post('/', uploadProductImage.single('imagen'), validateCreateProduct, createProduct);
 router.put('/:id', uploadProductImage.single('imagen'), validateProductId, updateProduct);
-
+router.patch('/:id/status', validateProductId, changeProductStatus);
 export default router;
